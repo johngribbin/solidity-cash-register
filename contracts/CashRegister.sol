d4342ab59f32737f32ff4d4ec88189dadfeec2a5
John Gribbin <johnggribbin@gmail.com>
2018-09-20 15:42:45 -0400

    add more tests for CashRegister contract

--- contracts/CashRegister.sol
+++ contracts/CashRegister.sol
@@ -1,9 +1,10 @@
 pragma solidity ^0.4.23;
 
 contract CashRegister {
+
     event NewReceipt(
-        address indexed _purchaserAddress,
-        uint indexed _receiptId
+        address indexed _purchaser,
+        uint indexed _receiptID
     );
 
     // Owner of the 'store' that operates the cash register
@@ -11,11 +12,10 @@ contract CashRegister {
     // Receives itemName as a string and returns the price of the item as a uint
     mapping(bytes32 => uint) public items; 
     // Receives receiptID as a unint and returns the specific Receipt struct
-    mapping(uint  => Receipt) receipts;
-    // receiptNonce is incremented to assign a unique receiptID to each receipt struct
+    mapping(uint => Receipt) public receipts;
+    // receiptNonce is incremented and used to assign a unique receiptID to each receipt
     uint receiptNonce;
-
-    // Each ndividual receipts takes the form of a struct
+    // Each individual receipt takes the form of a struct
     struct Receipt {
         address purchaser;
         uint receiptID;
@@ -23,37 +23,35 @@ contract CashRegister {
         bool finished;
     }
 
-    // set the address of the 'owner' to be the person who initializes the contract
+    // Set the address of the 'owner' to be the person who initializes the contract
     constructor() public {
         owner = msg.sender;
     }
 
-    // when added to a function, it can only be called by the owner
+    // When added to a function, it can only be called by the owner
     modifier restricted() {
-        require(msg.sender == owner, "Only the owner call call this function");
+        require(msg.sender == owner, "Only the owner can call this function");
         _;
     }
 
     // used by owner to add items to his/her store to make them available to a purchaser
-    // requires itemName a string, and itemPrice as a uint
-    function addItem(string itemName, uint itemPrice) public restricted {
+    function addItem(string _itemName, uint _itemPrice) public restricted {
         // any item added must have a price more than 0, otherwise it will not be found in the items mapping 
-        require(itemPrice > 0, "itemPrice must be more than 0"); 
+        require(_itemPrice > 0, "itemPrice must be more than 0"); 
         // adding the item to the items mapping
         // The itemName is the key, and the itemPrice is the value returned
-        items[keccak256(abi.encodePacked(itemName))] = itemPrice;
+        items[keccak256(abi.encodePacked(_itemName))] = _itemPrice;
     }
 
     // used by the owner to create a new receipt, and in the process emit a NewReceipt event
-    function newReceipt(address purchaser) public restricted {
-        // which includes the purchasers address and a unique ID for the receipt
+    function newReceipt(address _purchaser) public restricted {
         // create a unique receiptID for the receipt by incrementing receiptNonce by 1
         receiptNonce += 1;
         // emit a NewReceipt event, passing in the purchaser and the incremented receiptNonce 
-        emit NewReceipt(purchaser, receiptNonce);
-        // create new receipt utilixing the Receipt struct
-        // add the new receipt to the receipts mapping, with requires values assigned to the variables
-        receipts[receiptNonce] = Receipt({purchaser: purchaser, receiptID: receiptNonce, totalPrice: 0, finished: false});
+        emit NewReceipt(_purchaser, receiptNonce);
+        // create new receipt utilizing the Receipt struct
+        // add the receipt to the receipts smapping, passing the values assigned to be assigned to the variables
+        receipts[receiptNonce] = Receipt({purchaser: _purchaser, receiptID: receiptNonce, totalPrice: 0, finished: false});
     }
 
     // used by the purchaser (receipt owner) to add items to their specifc receipt
@@ -86,5 +84,4 @@ contract CashRegister {
         // return the totalPrice rung up on the receipt and the purchasers address
         return (r.totalPrice, r.purchaser);
     }
-
 }
--- test/TestCashRegister.js
+++ test/TestCashRegister.js
@@ -1,17 +1,135 @@
 const CashRegister = artifacts.require('CashRegister');
 
 contract('CashRegister', async(accounts) => {
+    let instance;
+    let manager;
+    let purchaser;
 
-    it('allows the owner to add items to the stores register', async() => {
-        let instance = await CashRegister.deployed();
+    before(async() => {
+        instance = await CashRegister.deployed();
+        manager = accounts[0];
+        purchaser = accounts[1];
+    });
 
+    it('Prevents someone who is not the owner from adding an item to the store', async() => {
         const itemName = "apple";
         const setPrice = 5;
+        // Try adding the apple and its price to the items mapping
+        try {
+            // Sending the transaction from account 1 (not the manager)
+            await instance.addItem(itemName, setPrice, {from: purchaser });
+        } catch (err) {
+            // check the error includes the word 'revert'
+            assert(err.toString().includes('revert'));
+            return;
+        }
+        assert(false, 'item was added to the stores register by someone other than the manager');
+    })
 
-        await instance.addItem(itemName, setPrice);
-
+    it('Allows the owner to add items to the stores register', async() => {
+        const itemName = "apple";
+        const setPrice = 5;
+        // add the apple and its price to the items mapping
+        await instance.addItem(itemName, setPrice );
+        // retrive the price of the apple from the items mapping
         const actualPrice = await instance.items.call(web3.sha3(itemName));
 
         assert.equal(setPrice, actualPrice, 'setPrice is not the same as actualPrice');
     })
+
+    
+    it('Creates a new receipt that includes the purchasers address and a unique receiptID', async() => {
+        // create a new receipt using account 1
+        await instance.newReceipt(purchaser);
+        // the receiptNonce will incremented from 0 to 1, so we expect receiptID to be 1
+        const testID = 1;
+        // retrive the receipt struct from the receipts mapping
+        const receiptStruct = await instance.receipts.call(testID);
+
+        // retrieve address and receiptID from the struct
+        const purchaserAddress = receiptStruct[0].toString();
+        const receiptID = receiptStruct[1].toString();
+        
+        assert.equal(testID, receiptID, 'testId is not the same as receiptID found on receipt');
+        assert.equal(purchaser, purchaserAddress, 'account 1 address is not the same as purchaserAddress found on receipt');
+    });
+
+    
+    it('Allow the purchaser to add items to their receipt', async() => {
+        const itemName = 'apple';
+        const setPrice = 5;
+
+        // add the apple and its price to the items mapping
+        await instance.addItem(itemName, setPrice ); 
+        // create a new receipt using account 1
+        await instance.newReceipt(purchaser);
+        // the receiptNonce will incremented from 0 to 1, so we expect receiptID to be 1
+        const testID = 1;
+
+        await instance.ringUpItem(testID, itemName, { from: purchaser });
+
+        // retrive the receipt struct from the receipts mapping
+        const receiptStruct = await instance.receipts.call(testID);
+        const totalPrice = receiptStruct[2].toString();
+
+        assert.equal(setPrice, totalPrice, 'setPrice is not the same as totalPrice on the receipt');
+    })
+
+    it('Prevents anyone other than the purchaser from adding items to their receipt', async() => {
+        const itemName = 'apple';
+        const setPrice = 5;
+        // add the apple and its price to the items mapping
+        await instance.addItem(itemName, setPrice ); 
+        // create a new receipt using account 1
+        await instance.newReceipt(purchaser);
+        // the receiptNonce will incremented from 0 to 1, so we expect receiptID to be 1
+        const testID = 1;
+        // try sending transaction from the managers address
+        try {
+            await instance.ringUpItem(testID, itemName, { from: manager });
+        } catch(err) {
+            // check the error includes the word 'revert'
+            assert(err.toString().includes('revert'));
+            return;
+        }
+        assert(false, 'allows the manager to add an item to the purchasers receipt');
+    })
+
+    it('Allows the manager to finalize a receipt', async() => {
+        // create a receipt
+        await instance.newReceipt(purchaser);
+
+        const testID = 1;
+
+        await instance.finishReceipt(testID);
+
+        // retrive the receipt struct from the receipts mapping
+        const receiptStruct = await instance.receipts.call(testID);
+
+        const finished = receiptStruct[3].toString();
+
+        assert(finished, true, 'did not change finished variable to true');
+    })
+
+    it('Should return purchasers address and total price of a given receipt when viewReceipt is called', async() => {
+        const itemName = 'apple';
+        const setPrice = 5;
+
+        // add the apple and its price to the items mapping
+        await instance.addItem(itemName, setPrice ); 
+        // create a new receipt using account 1
+        const transReceipt = await instance.newReceipt(purchaser);
+
+        let _receiptID = transReceipt.logs[0].args._receiptID.toString();
+        
+        await instance.ringUpItem(_receiptID, itemName, { from: purchaser });
+
+        const returnValues = await instance.viewReceipt.call(_receiptID);
+
+        const [returnedTotalPrice, returnedPurchaser] = returnValues;
+        
+        assert.equal(returnedTotalPrice, setPrice, 'totalPrice returned from viewReceipt is not the same as setPrice');
+        assert.equal(returnedPurchaser, purchaser, 'purchaser returned from viewReceipt is not the same as purchaser');
+    })
+    
 });
